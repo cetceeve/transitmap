84f239dc327bad02fb2f22ebd31aacef48e18868
JonathanArns <jonathan.arns@googlemail.com>
2024-10-30 15:11:55 +0100

    fix metadata download latency and update dependencies

--- rust-crates/types/src/metadata/sync_table.rs
+++ rust-crates/types/src/metadata/sync_table.rs
@@ -1,28 +1,25 @@
-use std::{collections::HashMap, sync::{RwLock, Mutex}, time::{SystemTime, Duration}};
+use std::{collections::HashMap, sync::{Mutex, OnceLock, RwLock}, time::{Duration, SystemTime}};
 
 use super::{VehicleMetadata, download_metadata_table_blocking};
 
-static mut TABLE: Option<RwLock<HashMap<String, VehicleMetadata>>> = None;
-static mut LAST_UPDATED: Option<Mutex<SystemTime>> = None;
+static mut TABLE: OnceLock<RwLock<HashMap<String, VehicleMetadata>>> = OnceLock::new();
+static mut LAST_UPDATED: OnceLock<Mutex<SystemTime>> = OnceLock::new();
 
 fn ensure_table() {
     unsafe {
-        if let None = TABLE {
-            LAST_UPDATED = Some(Mutex::new(SystemTime::now()));
-            TABLE = Some(RwLock::new(Default::default()));
-            if let Some(ref table) = TABLE {
-                let mut lock = table.write().unwrap();
-                *lock = download_metadata_table_blocking().unwrap();
-            }
+        if TABLE.get().is_none() {
+            LAST_UPDATED.get_or_init(|| {Mutex::new(SystemTime::now())});
+            TABLE.get_or_init(|| { RwLock::new(Default::default()) });
+            let mut lock = TABLE.get().unwrap().write().unwrap();
+            *lock = download_metadata_table_blocking().unwrap();
         } else {
-            if let Some(ref last_updated) = LAST_UPDATED {
-                let mut ts = last_updated.lock().unwrap();
-                if ts.elapsed().unwrap() > Duration::from_secs(24*60*60) {
-                    *ts = SystemTime::now();
-                    if let Some(ref table) = TABLE {
-                        *table.write().unwrap() = download_metadata_table_blocking().unwrap();
-                    }
-                }
+            let last_updated = LAST_UPDATED.get_or_init(|| {Mutex::new(SystemTime::now())});
+            let mut ts = last_updated.lock().unwrap();
+            if ts.elapsed().unwrap() > Duration::from_secs(24*60*60) {
+                let table = TABLE.get_or_init(|| { RwLock::new(Default::default()) });
+                let data = download_metadata_table_blocking().unwrap();
+                *table.write().unwrap() = data;
+                *ts = SystemTime::now();
             }
         }
     }
@@ -35,7 +32,7 @@ pub fn init_blocking_metadata_table() {
 pub fn get_trip_metadata_blocking(trip_id: &str) -> Option<VehicleMetadata> {
     ensure_table();
     unsafe {
-        if let Some(ref table) = TABLE {
+        if let Some(table) = TABLE.get() {
             table.read().unwrap().get(trip_id).cloned()
         } else {
             None
